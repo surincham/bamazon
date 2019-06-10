4d9ec128f71caecc1e3e9e75f3b0e2595291e3c0
Surin Cham <oSuRiNo@Surins-MacBook-Pro.local>
2019-06-09 22:55:22 -0600

    final code and pictures

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -9,11 +9,48 @@ var connection = mysql.createConnection({
     database: "bamazon_db"
 });
 
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
+connection.connect(function (err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
 });
 
+connection.connect(function (err) {
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        console.log(res);
+        start();
+    });
+});
+
+function start() {
+    inquirer
+        .prompt({
+            name: 'purchase',
+            type: 'rawlist',
+            message: 'What would you like to purchase? ',
+            choices: ['Reusable Straws', 
+            'Smart Television', 
+            'MacBook Pro', 
+            'Makeup', 
+            'Pre-Workout Supplement', 
+            'Nintendo Switch', 
+            'Bose Over-the-ear Headphones (Noise Cancellation)', 
+            'Apple Watch (Series 3)', 
+            'HP All-In-One Printer', 
+            'Texas Instrument Calculator']
+        })
+        quantity();
+};
+
+function quantity() {
+    inquirer
+        .prompt({
+            name: 'amount',
+            type: 'input',
+            message: 'How many would you like to purchase? '
+            // transformer: 'Please enter a valid number'
+        })
+}
